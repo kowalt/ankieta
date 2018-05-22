aca60889066cb236bd4791840c0385c37c2a8194
Radek <tede@tedes-macbook.home>
2018-05-22 15:42:29 +0200

    modyfikacja nazw katalogow

---
+++ App/PollBE/src/main/java/pl/jodkow/PollBE/PollBeApplication.java
@@ -0,0 +1,12 @@
+package pl.jodkow.PollBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PollBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PollBeApplication.class, args);
+	}
+}
